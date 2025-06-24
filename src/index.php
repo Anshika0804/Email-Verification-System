c8033f85439f24cfd7b3a34c40b5707d379e5605
Anshika Rai <anshikacoder10@gmail.com>
2025-06-24 16:22:36 +0530

    Improved form UX: retain email and verification code fields after submit

--- src/index.php
+++ src/index.php
@@ -34,13 +34,13 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <h2>Subscribe to Daily XKCD Comic</h2>
     <form method="POST">
         <label>Email:</label><br>
-        <input type="email" name="email" required>
-        <button id="submit-email">Submit</button>
-        <br><br>
+            <input type="email" name="email" required value="<?= isset($_POST['email']) ? htmlspecialchars($_POST['email']) : '' ?>">
+            <button id="submit-email">Submit</button>
+            <br><br>
 
-        <label>Verification Code:</label><br>
-        <input type="text" name="verification_code" maxlength="6">
-        <button id="submit-verification">Verify</button>
+            <label>Verification Code:</label><br>
+            <input type="text" name="verification_code" maxlength="6" value="<?= isset($_POST['verification_code']) ? htmlspecialchars($_POST['verification_code']) : '' ?>">
+            <button id="submit-verification">Verify</button>
     </form>
 
     <?php if ($message): ?>
